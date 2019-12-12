8fc8df360d40c9ca00585f6a0f2d4e593946436e
Thomas Beaman <thomasthekingmaker@gmail.com>
2019-12-12 09:42:21 -0500

    another heroku attempt

--- Workout/server.js
+++ Workout/server.js
@@ -15,8 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workoutdb";
 mongoose.connect(MONGODB_URI);
 
 app.get("/get", (req, res) => {
